1e27bfb96cbc28df5c46ce9ae83517223105b0c6
AlexanderKhasanov <sasha_020398@mail.ru>
2017-12-10 16:26:03 +0300

    update cascades

--- haar_cascades/work_with_cascades.py
+++ haar_cascades/work_with_cascades.py
@@ -5,12 +5,26 @@ import numpy as np
 from PIL import Image
 
 def recognize(photo, cascades):
+	number_classes = 21
 	all_cascades = {}
-	numbers = []
+	numbers = {}
+	#create cascades
 	for key, name_cascade in cascades:
-		new_cascade = cv2.CascadeClassifier(name_cascade)
-		all_cascades.update([key, new_cascade])
-	recognizer = cv2.createLBPHFaceRecognizer(1,8,8,8,123)
-	for i in range(21):
-		numbers.append( all_cascades[i].detectMultiScale(image, scaleFactor=1.1, minNeighbors=5, minSize=(10, 15)) )
-	#for (x, y, w, h) in numbers:
+		all_cascades.update([key, cv2.CascadeClassifier(name_cascade)])
+	#recognizer = cv2.createLBPHFaceRecognizer(1,8,8,8,123)
+
+	#Create an image and bring it to an array
+	gray = Image.open(photo).convert('L')
+	image = np.array( gray, 'uint8' )
+
+	#Each cascade checks the image
+	for i in range(number_classes):
+		coordinates = all_cascades[i].detectMultiScale(image, scaleFactor=1.1, minNeighbors=5)
+		#if the cascade has found something, then add it to the dictionary and display it on the screen
+		if len(coordinates) != 0:
+			numbers.update([i, coordinates])
+			for (x, y, w, h) in faces:
+				cv2.imshow("", image[y: y + h, x: x + w])
+				cv2.waitKey(50)
+	return numbers
+
